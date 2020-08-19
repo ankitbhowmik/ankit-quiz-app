373008838000f9cf6e979676547254a7db10950b
ankit <ankibhowmik@gmail.com>
2020-08-19 11:39:48 +0530

    Updates

---
+++ precache-manifest.516e1c4e80b877f5733c8783a0ce3450.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b409bef81fa7f9cc1fd8c764efca90",
+    "url": "/ankit-quiz-app/index.html"
+  },
+  {
+    "revision": "e07d9c96d80ade995fc8",
+    "url": "/ankit-quiz-app/static/css/main.6b6f8d75.chunk.css"
+  },
+  {
+    "revision": "ca9eddb031a614efeb97",
+    "url": "/ankit-quiz-app/static/js/2.b86df4a9.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/ankit-quiz-app/static/js/2.b86df4a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07d9c96d80ade995fc8",
+    "url": "/ankit-quiz-app/static/js/main.f7314210.chunk.js"
+  },
+  {
+    "revision": "7eee1049dec9417adbd0",
+    "url": "/ankit-quiz-app/static/js/runtime-main.16d7488d.js"
+  },
+  {
+    "revision": "02882ce29c39d46d8cb8c0eac28ce519",
+    "url": "/ankit-quiz-app/static/media/Correct-answer.02882ce2.mp3"
+  },
+  {
+    "revision": "093eed4f4091d0792b60b33a1d98448c",
+    "url": "/ankit-quiz-app/static/media/Wrong-answer.093eed4f.mp3"
+  }
+]);
